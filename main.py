70b7cb87f320d7b23beaa2edd2121fe6efd59eba
周恭煥 <65105273+kenchou2006@users.noreply.github.com>
2024-08-08 20:49:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import secrets
 
-RANDOM_STRING_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-def get_random_string(length, allowed_chars=RANDOM_STRING_CHARS):
-    return "".join(secrets.choice(allowed_chars) for i in range(length))
-
 def get_random_secret_key():
-    chars = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
-    return (f"django-insecure-{get_random_string(50, chars)}")
+    return f"django-insecure-{''.join(secrets.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for i in range(50))}"
 
 print(get_random_secret_key())
